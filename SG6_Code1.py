848ca7b1390c88e07009c44f3c69f07feeac2a65
MarvounLCoranes <marvoun.coranes@smc.pshs.edu.ph>
2025-09-10 08:59:13 +0800

    Sum of Three Numbers with Decimals
    The code asks the user for three numbers with decimals using float and adds them. It also rounds the sum to two decimal places.

---
+++ SG6_Code1.py
@@ -0,0 +1,6 @@
+num1= float(input("Enter first number: "))
+num2= float(input("Enter second number: "))
+num3= float(input("Enter third number: "))
+sum= num1+num2+num3
+round_sum= round(sum,2)
+print("The total of the three numbers is:", round_sum)
